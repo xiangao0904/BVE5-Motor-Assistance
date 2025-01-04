da9a9ebf408105480fae3f4462eb2c8f43896c0c
huliawsl <3150399699@qq.com>
2025-01-04 21:32:43 +0800

    实现动态矩形宽度

--- Avalonia-BVE5-Motor-Assistance/Controls/CustomButton1.axaml.cs
+++ Avalonia-BVE5-Motor-Assistance/Controls/CustomButton1.axaml.cs
@@ -1,12 +1,44 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
-using Avalonia.Media;
+using Avalonia.VisualTree;
 
-namespace Avalonia_BVE5_Motor_Assistance.Controls;
-
-public class CustomButton1 : RadioButton
+namespace Avalonia_BVE5_Motor_Assistance.Controls
 {
+    public class CustomButton1 : RadioButton
+    {
+        public static readonly StyledProperty<int> RectangleWidthProperty =
+            AvaloniaProperty.Register<CustomButton1, int>(nameof(RectangleWidth), defaultValue: 4);
 
+        public int RectangleWidth
+        {
+            get => GetValue(RectangleWidthProperty);
+            set => SetValue(RectangleWidthProperty, value);
+        }
 
+        public CustomButton1()
+        {
+            // 订阅 Loaded 事件
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, EventArgs e)
+        {
+            if (this.GetVisualRoot() is not Window window) return;
+
+            // 监听窗口大小变化
+            window.SizeChanged += (s, args) =>
+            {
+                if (window.WindowState == WindowState.Maximized)
+                {
+                    RectangleWidth = 10; // 设定全屏时的宽度
+                }
+                else
+                {
+                    RectangleWidth = 4; // 设定正常状态下的宽度
+                }
+            };
+        }
+    }
 }
